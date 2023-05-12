dc031417e830ed8ca2e210fd19f9f0c3ba3a1cb8
Denis Pipko <dzianis.pipko@gmail.com>
2023-05-12 23:08:45 +0300

    adding amination on scroll navigation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,80 @@
-const owl = $('.owl-carousel');
-owl.owlCarousel({
-	center: true,
-	loop: true,
-	margin: 30,
-	startPosition: 1,
-	items: 3,
-  });
-// Go to the previous item
-$('.slider__btn--prev').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl.trigger('prev.owl.carousel', [300]);
-})
-// Go to the next item
-$('.slider__btn--next').click(function() {
-    owl.trigger('next.owl.carousel');
-})
+// Scroll to anchors
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.header__nav').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
+
+// Scroll to header
+(function () {
+	const header = document.querySelector('.header__nav');
+	window.onscroll = () => {
+		if(window.pageYOffset > 50) {
+			header.classList.add('header__nav--active');
+		} else {
+			header.classList.remove('header__nav--active');
+		}
+	};
+}());
+
+// Toggle burger menu
+(function () {
+	const burgerItem = document.querySelector('.burger');
+	const menu = document.querySelector('.header__nav');
+	const menuCloseItem = document.querySelector('.header__nav-close');
+	const mobileMenuLink = document.querySelectorAll('.header__link');
+
+	burgerItem.addEventListener('click', () => {
+		menu.classList.add('header__nav_active');
+	})
+
+	menuCloseItem.addEventListener('click', () => {
+		menu.classList.remove('header__nav_active');
+	})
+
+	if(window.innerWidth <= 767){
+		for(let i = 0; i < mobileMenuLink.length; i++){
+			mobileMenuLink[i].addEventListener('click', () => {
+				menu.classList.remove('header__nav_active');
+			})
+		}
+	}
+}())
+
+
+
+
+
